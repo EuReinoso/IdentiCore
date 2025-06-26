806387f99fb92a72b1b190036119b630df4fb9f7
Reinoso <77119687+EuReinoso@users.noreply.github.com>
2025-06-26 15:31:42 -0300

    feat(Application): Add Services

---
+++ Application/Services/AddressService.cs
@@ -0,0 +1,65 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services;
+
+public class AddressService : IAddressService
+{
+    private readonly IAddressRepository _addressRepository;
+    private readonly IClientRepository _clientRepository;
+
+    public AddressService(IAddressRepository addressRepository, IClientRepository clientRepository)
+    {
+        _addressRepository = addressRepository;
+        _clientRepository = clientRepository;
+    }
+
+    public async Task<IEnumerable<Address>> GetAllAsync()
+    {
+        return await _addressRepository.GetAllAsync();
+    }
+
+    public async Task<IEnumerable<Address>> GetByClientIdAsync(Guid clientId)
+    {
+        return await _addressRepository.GetByClientIdAsync(clientId);
+    }
+
+    public async Task<Address?> GetByIdAsync(Guid id)
+    {
+        return await _addressRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Address> CreateAsync(Address address)
+    {
+        var client = await _clientRepository.GetByIdAsync(address.ClientId);
+        if (client == null)
+            throw new InvalidOperationException("Client not found.");
+
+        await _addressRepository.AddAsync(address);
+        return address;
+    }
+
+    public async Task<Address> UpdateAsync(Address address)
+    {
+        var existing = await _addressRepository.GetByIdAsync(address.Id);
+        if (existing == null)
+            throw new KeyNotFoundException("Address not found.");
+
+        await _addressRepository.UpdateAsync(address);
+        return address;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var address = await _addressRepository.GetByIdAsync(id);
+        if (address == null)
+            throw new KeyNotFoundException("Address not found.");
+
+        await _addressRepository.DeleteAsync(address);
+    }
+}
---
+++ Application/Services/ClientService.cs
@@ -0,0 +1,56 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Application.Services;
+public class ClientService : IClientService
+{
+    private readonly IClientRepository _clientRepository;
+
+    public ClientService(IClientRepository clientRepository)
+    {
+        _clientRepository = clientRepository;
+    }
+
+    public async Task<IEnumerable<Client>> GetAllAsync()
+    {
+        return await _clientRepository.GetAllAsync();
+    }
+
+    public async Task<Client?> GetByIdAsync(Guid id)
+    {
+        return await _clientRepository.GetByIdAsync(id);
+    }
+
+    public async Task<Client> CreateAsync(Client client)
+    {
+        var emailExists = await _clientRepository.EmailExistsAsync(client.Email);
+        if (emailExists)
+            throw new InvalidOperationException("Email is already in use.");
+
+        await _clientRepository.AddAsync(client);
+        return client;
+    }
+
+    public async Task<Client> UpdateAsync(Client client)
+    {
+        var existing = await _clientRepository.GetByIdAsync(client.Id);
+        if (existing == null)
+            throw new KeyNotFoundException("Client not found.");
+
+        var emailInUse = await _clientRepository.EmailExistsAsync(client.Email, client.Id);
+        if (emailInUse)
+            throw new InvalidOperationException("Email is already in use by another client.");
+
+        await _clientRepository.UpdateAsync(client);
+        return client;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var client = await _clientRepository.GetByIdAsync(id);
+        if (client == null)
+            throw new KeyNotFoundException("Client not found.");
+
+        await _clientRepository.DeleteAsync(client);
+    }
+}
